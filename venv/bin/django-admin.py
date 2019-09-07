074a609e979aa40b63f699b8c7199d2173e5fe3f
stephenyoon84 <stephenyoon84@gmail.com>
2019-09-07 18:02:34 -0400

    init

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/StephenYOON/Desktop/Self_Project/Django/My-Portfolio/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
